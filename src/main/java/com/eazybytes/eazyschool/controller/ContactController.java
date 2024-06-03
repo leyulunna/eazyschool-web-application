f549c02f48e99024acb8bf66ed18b5b45a815d2c
Lena Zhang <lena.zhang@tpisoftware.com>
2024-06-03 09:08:27 -0700

    feat: Model Contact - Validator

--- src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -2,13 +2,18 @@ package com.eazybytes.eazyschool.controller;
 
 import com.eazybytes.eazyschool.model.Contact;
 import com.eazybytes.eazyschool.service.ContactService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import lombok.extern.slf4j.Slf4j;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+@Slf4j
 @Controller
 public class ContactController {
 
@@ -20,13 +25,20 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
+
         return "contact.html";
     }
 
-    @RequestMapping(value = "/saveMsg",  method = POST)
-    public ModelAndView saveMsg(Contact contact) {
+    @RequestMapping(value = "/saveMsg",method = POST)
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors){
+
+        if(errors.hasErrors()){
+            log.error("Contact form validation failed due to : " + errors.toString());
+            return "contact.html";
+        }
         contactService.saveMsgDetail(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 }
